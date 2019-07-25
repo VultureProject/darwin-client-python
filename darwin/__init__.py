2e9d453ebeb6ba9ff612a9c66660125e2228bcae
Guillaume Catto <guillaume.catto@advens.fr>
2019-07-25 12:25:32 +0200

    Improve comments and version

--- darwin/__init__.py
+++ darwin/__init__.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+
+"""__init__.py: Import classes used in the Darwin Python package"""
+
+__author__ = "gcatto"
+__version__ = "2.0"
+__date__ = "20/02/19"
+__license__ = "GPLv3"
+__copyright__ = "Copyright (c) 2019 Advens. All rights reserved."
+
 from .darwinapi import DarwinApi
 
 from .darwinexceptions import (
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="darwin",
-    version="0.2",
+    version="2.0",
     description="Call Darwin with your Python code!",
     url="https://github.com/VultureProject/darwin-client-python",
     author="Guillaume Catto",
