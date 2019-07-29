a09f66ad8c423de6a8205d4758faf904349b9869
Guillaume Catto <guillaume.catto@advens.fr>
2019-07-29 10:39:41 +0200

    Improve logs

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -32,70 +32,298 @@ class colors:
     UNDERLINE = '\033[4m'
 
 
-def log(function_name, filter_name, message, color=colors.HEADER):
+def log(function_name, filter_name, message, color=colors.HEADER, is_indent=False):
+    if is_indent:
+        indent = "\t"
+
+    else:
+        indent = ""
+
+    if filter_name is not None:
+        filter_header = "{filter_name} filter: ".format(filter_name=filter_name, )
+
+    else:
+        filter_header = ""
+
     print(
-        "{color_begin}{module_name}:: {function_name}:: \t{filter_name} filter: {message}{color_end}".format(
+        "{color_begin}{module_name}:: {function_name}:: {indent}{filter_header}{message}{color_end}".format(
             color_begin=color,
             module_name=MODULE_NAME,
             function_name=function_name,
-            filter_name=filter_name,
+            filter_header=filter_header,
             message=message,
             color_end=colors.END,
+            indent=indent,
         )
     )
 
 
 def test_filter(filter_name, socket_type=None, socket_path=None, socket_host=None, socket_port=None, filter_code=None,
                 response_type="back", verbose=True, debug_mode=False, **kwargs):
-    call_args = kwargs.get("call_args", None)
-    bulk_call_args = kwargs.get("bulk_call_args", None)
-    expected_bulk_results = kwargs.get("expected_bulk_results", None)
-
-    # expected_call_result could REALLY be None, so we have to make the distinction
     try:
-        expected_call_result = kwargs["expected_call_result"]
-        is_expected_call_result = True
-    except KeyError:
-        expected_call_result = None
-        is_expected_call_result = False
+        call_args = kwargs.get("call_args", None)
+        bulk_call_args = kwargs.get("bulk_call_args", None)
+        expected_bulk_results = kwargs.get("expected_bulk_results", None)
 
-    if debug_mode:
-        print("{color_begin}{module_name}:: test_filter:: Testing {filter_name} filter{color_end}".format(
-            filter_name=filter_name, module_name=MODULE_NAME, color_begin=colors.OKBLUE, color_end=colors.END,
-        ))
+        # expected_call_result could REALLY be None, so we have to make the distinction
+        try:
+            expected_call_result = kwargs["expected_call_result"]
+            is_expected_call_result = True
+        except KeyError:
+            expected_call_result = None
+            is_expected_call_result = False
 
-    # if not filter code is provided, we try to get it from the filter name
-    if filter_code is None:
         if debug_mode:
             log(
                 "test_filter",
                 filter_name,
-                "no filter code provided, so it will be extracted from the filter name \"{filter_name}\"",
+                "tests will begin",
+                color=colors.OKBLUE,
+            )
+
+        # if not filter code is provided, we try to get it from the filter name
+        if filter_code is None:
+            if debug_mode:
+                log(
+                    "test_filter",
+                    filter_name,
+                    "no filter code provided, so it will be extracted from the filter name \"{filter_name}\"",
+                    color=colors.WARNING,
+                    is_indent=True,
+                )
+
+            filter_code = filter_name
+
+        try:
+            if socket_type == "unix":
+                darwin_api = DarwinApi(socket_path=socket_path,
+                                       socket_type=socket_type,
+                                       verbose=verbose, )
+
+            elif socket_type == "tcp":
+                darwin_api = DarwinApi(socket_host=socket_host,
+                                       socket_port=socket_port,
+                                       verbose=verbose, )
+
+            else:
+                if debug_mode:
+                    log(
+                        "test_filter",
+                        filter_name,
+                        "Invalid socket type provided: \"{socket_type}\"".format(socket_type=socket_type),
+                        color=colors.FAIL,
+                        is_indent=True,
+                    )
+
+                log(
+                    "test_filter",
+                    filter_name,
+                    "[NOT OK]",
+                    color=colors.FAIL,
+                    is_indent=True,
+                )
+
+                return
+
+        except DarwinConnectionError:
+            if debug_mode:
+                log(
+                    "test_filter",
+                    filter_name,
+                    "the filter does not appear to be running",
+                    color=colors.WARNING,
+                    is_indent=True,
+                )
+
+            log(
+                "test_filter",
+                filter_name,
+                "[NOT RUNNING]",
                 color=colors.WARNING,
             )
 
-        filter_code = filter_name
+            return
 
-    try:
-        if socket_type == "unix":
-            darwin_api = DarwinApi(socket_path=socket_path,
-                                   socket_type=socket_type,
-                                   verbose=verbose, )
+        is_ok = True
 
-        elif socket_type == "tcp":
-            darwin_api = DarwinApi(socket_host=socket_host,
-                                   socket_port=socket_port,
-                                   verbose=verbose, )
+        if call_args is not None:
+            if debug_mode:
+                log(
+                    "test_filter",
+                    filter_name,
+                    "calling call function",
+                    color=colors.HEADER,
+                    is_indent=True,
+                )
+
+            darwin_result = darwin_api.call(
+                call_args,
+                filter_code=filter_code,
+                response_type=response_type,
+            )
+
+            if is_expected_call_result:
+                if expected_call_result != darwin_result:
+                    if debug_mode:
+                        log(
+                            "test_filter",
+                            filter_name,
+                            "call function: expected result not matching Darwin: "
+                            "{expected_result} != {darwin_result}".format(
+                                expected_result=expected_call_result, darwin_result=darwin_result,
+                            ),
+                            color=colors.FAIL,
+                            is_indent=True,
+                        )
+
+                    is_ok = False
+
+                else:
+                    if debug_mode:
+                        log(
+                            "test_filter",
+                            filter_name,
+                            "call function: expected result matches Darwin: {expected_result}".format(
+                                expected_result=expected_call_result,
+                            ),
+                            color=colors.OKGREEN,
+                            is_indent=True,
+                        )
+
+            else:
+                if debug_mode:
+                    log(
+                        "test_filter",
+                        filter_name,
+                        "call function: expected_call_result is not provided: "
+                        "nothing to check. Darwin result obtained: {darwin_result}".format(
+                            darwin_result=darwin_result,
+                        ),
+                        color=colors.OKGREEN,
+                        is_indent=True,
+                    )
+
+            if debug_mode:
+                log(
+                    "test_filter",
+                    filter_name,
+                    "call function ended",
+                    color=colors.OKBLUE,
+                    is_indent=True,
+                )
 
         else:
             if debug_mode:
                 log(
                     "test_filter",
                     filter_name,
-                    "Invalid socket type provided: \"{socket_type}\"".format(socket_type=socket_type),
-                    color=colors.FAIL,
+                    "call_args is None: nothing to do".format(
+                        expected_result=expected_call_result,
+                    ),
+                    color=colors.HEADER,
+                    is_indent=True,
                 )
 
+        if bulk_call_args is not None:
+            if debug_mode:
+                log(
+                    "test_filter",
+                    filter_name,
+                    "calling bulk_call function".format(
+                        expected_result=expected_call_result,
+                    ),
+                    color=colors.HEADER,
+                    is_indent=True,
+                )
+
+            darwin_result = darwin_api.bulk_call(
+                bulk_call_args,
+                filter_code=filter_code,
+                response_type=response_type,
+            )
+
+            if expected_bulk_results is not None:
+                if expected_bulk_results != darwin_result["certitude_list"]:
+                    if debug_mode:
+                        log(
+                            "test_filter",
+                            filter_name,
+                            "bulk_call function: "
+                            "expected result not matching Darwin: {expected_result} != {darwin_result}".format(
+                                expected_result=expected_bulk_results,
+                                darwin_result=darwin_result["certitude_list"],
+                            ),
+                            color=colors.FAIL,
+                            is_indent=True,
+                        )
+
+                    is_ok = False
+
+                else:
+                    if debug_mode:
+                        log(
+                            "test_filter",
+                            filter_name,
+                            "bulk_call function: "
+                            "expected result matches Darwin: {expected_result}".format(
+                                expected_result=expected_bulk_results,
+                            ),
+                            color=colors.OKGREEN,
+                            is_indent=True,
+                        )
+
+            else:
+                if debug_mode:
+                    log(
+                        "test_filter",
+                        filter_name,
+                        "bulk_call function: "
+                        "expected_bulk_results is None: nothing to check. "
+                        "Darwin result obtained: {darwin_result}".format(
+                            darwin_result=darwin_result["certitude_list"],
+                        ),
+                        color=colors.OKGREEN,
+                        is_indent=True,
+                    )
+
+            if debug_mode:
+                log(
+                    "test_filter",
+                    filter_name,
+                    "bulk_call function ended",
+                    color=colors.HEADER,
+                    is_indent=True,
+                )
+
+        else:
+            if debug_mode:
+                log(
+                    "test_filter",
+                    filter_name,
+                    "bulk_call_args is None: nothing to do",
+                    color=colors.HEADER,
+                    is_indent=True,
+                )
+
+        darwin_api.close()
+
+        if debug_mode:
+            log(
+                "test_filter",
+                filter_name,
+                "tests are finished",
+                color=colors.OKBLUE,
+            )
+
+        if is_ok:
+            log(
+                "test_filter",
+                filter_name,
+                "[OK]",
+                color=colors.OKGREEN,
+            )
+
+        else:
             log(
                 "test_filter",
                 filter_name,
@@ -103,221 +331,46 @@ def test_filter(filter_name, socket_type=None, socket_path=None, socket_host=Non
                 color=colors.FAIL,
             )
 
-            return
-
-    except DarwinConnectionError:
+    except Exception as error:
         if debug_mode:
-            log(
-                "test_filter",
-                filter_name,
-                "the filter does not appear to be running",
-                color=colors.WARNING,
-            )
+            raise
 
         log(
             "test_filter",
             filter_name,
-            "[NOT RUNNING]",
-            color=colors.WARNING,
-        )
-
-        return
-
-    is_ok = True
-
-    if call_args is not None:
-        if debug_mode:
-            log(
-                "test_filter",
-                filter_name,
-                "calling call function",
-                color=colors.HEADER,
-            )
-
-        darwin_result = darwin_api.call(
-            call_args,
-            filter_code=filter_code,
-            response_type=response_type,
-        )
-
-        if is_expected_call_result:
-            if expected_call_result != darwin_result:
-                if debug_mode:
-                    log(
-                        "test_filter",
-                        filter_name,
-                        "call function: expected result not matching Darwin: "
-                        "{expected_result} != {darwin_result}".format(
-                            expected_result=expected_call_result, darwin_result=darwin_result,
-                        ),
-                        color=colors.FAIL,
-                    )
-
-                is_ok = False
-
-            else:
-                if debug_mode:
-                    log(
-                        "test_filter",
-                        filter_name,
-                        "call function: expected result matches Darwin: {expected_result}".format(
-                            expected_result=expected_call_result,
-                        ),
-                        color=colors.OKGREEN,
-                    )
-
-        else:
-            if debug_mode:
-                log(
-                    "test_filter",
-                    filter_name,
-                    "call function: expected_call_result is not provided: "
-                    "nothing to check. Darwin result obtained: {darwin_result}".format(
-                        darwin_result=darwin_result,
-                    ),
-                    color=colors.OKGREEN,
-                )
-
-        if debug_mode:
-            log(
-                "test_filter",
-                filter_name,
-                "call function ended",
-                color=colors.OKBLUE,
-            )
-
-    else:
-        if debug_mode:
-            log(
-                "test_filter",
-                filter_name,
-                "call_args is None: nothing to do".format(
-                    expected_result=expected_call_result,
-                ),
-                color=colors.HEADER,
-            )
-
-    if bulk_call_args is not None:
-        if debug_mode:
-            log(
-                "test_filter",
-                filter_name,
-                "calling bulk_call function".format(
-                    expected_result=expected_call_result,
-                ),
-                color=colors.HEADER,
-            )
-
-        darwin_result = darwin_api.bulk_call(
-            bulk_call_args,
-            filter_code=filter_code,
-            response_type=response_type,
-        )
-
-        if expected_bulk_results is not None:
-            if expected_bulk_results != darwin_result["certitude_list"]:
-                if debug_mode:
-                    log(
-                        "test_filter",
-                        filter_name,
-                        "bulk_call function: "
-                        "expected result not matching Darwin: {expected_result} != {darwin_result}".format(
-                            expected_result=expected_bulk_results,
-                            darwin_result=darwin_result["certitude_list"],
-                        ),
-                        color=colors.FAIL,
-                    )
-
-                is_ok = False
-
-            else:
-                if debug_mode:
-                    log(
-                        "test_filter",
-                        filter_name,
-                        "bulk_call function: "
-                        "expected result matches Darwin: {expected_result}".format(
-                            expected_result=expected_bulk_results,
-                        ),
-                        color=colors.OKGREEN,
-                    )
-
-        else:
-            if debug_mode:
-                log(
-                    "test_filter",
-                    filter_name,
-                    "bulk_call function: "
-                    "expected_bulk_results is None: nothing to check. Darwin result obtained: {darwin_result}".format(
-                        darwin_result=darwin_result["certitude_list"],
-                    ),
-                    color=colors.OKGREEN,
-                )
-
-        if debug_mode:
-            log(
-                "test_filter",
-                filter_name,
-                "bulk_call function ended",
-                color=colors.HEADER,
-            )
-
-    else:
-        if debug_mode:
-            log(
-                "test_filter",
-                filter_name,
-                "bulk_call_args is None: nothing to do",
-                color=colors.HEADER,
-            )
-
-    darwin_api.close()
-
-    if debug_mode:
-        print(
-            "{color_begin}"
-            "{module_name}:: test_filter:: Tests with the {filter_name} filter are finished"
-            "{color_end}".format(
-                filter_name=filter_name, module_name=MODULE_NAME, color_begin=colors.OKBLUE, color_end=colors.END,
-            )
-        )
-
-    if is_ok:
-        log(
-            "test_filter",
-            filter_name,
-            "[OK]",
-            color=colors.OKGREEN,
-        )
-
-    else:
-        log(
-            "test_filter",
-            filter_name,
-            "[NOT OK]",
+            "[ERROR] {error}".format(error=error, ),
             color=colors.FAIL,
         )
 
 
 def run_tests(tests_descr, debug_mode=False):
     if debug_mode:
-        print("{color_begin}{module_name}:: run_tests:: Beginning tests...{color_end}".format(
-            module_name=MODULE_NAME, color_begin=colors.HEADER, color_end=colors.END,
-        ))
+        log(
+            "run_tests",
+            None,
+            "beginning tests...",
+            color=colors.HEADER,
+        )
 
     for test_descr in tests_descr:
         test_filter(debug_mode=debug_mode, **test_descr)
 
     if debug_mode:
-        print("{color_begin}{module_name}:: run_tests:: Tests ended{color_end}".format(
-            module_name=MODULE_NAME, color_begin=colors.HEADER, color_end=colors.END,
-        ))
+        log(
+            "run_tests",
+            None,
+            "test ended",
+            color=colors.HEADER,
+        )
 
 
 if __name__ == "__main__":
-    print("{color_begin}{module_name}:: __main__:: Tests will be run on Darwin filters{color_end}".format(
-        module_name=MODULE_NAME, color_begin=colors.HEADER, color_end=colors.END,
-    ))
+    log(
+        "__main__",
+        None,
+        "tests will be run on Darwin filters",
+        color=colors.HEADER,
+    )
 
     try:
         raw_arg = sys.argv[1]
