dfca44e2bf30b9a49b5e7475cb72cfd17c91bc30
Hugo Soszynski <hugo.soszynski@outlook.com>
2020-01-20 09:51:22 +0100

    Update Version to 1.1 (#17)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="darwin",
-    version="1.0",
+    version="1.1",
     description="Call Darwin with your Python code!",
     url="https://github.com/VultureProject/darwin-client-python",
     author="Guillaume Catto",
